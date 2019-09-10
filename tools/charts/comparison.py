2f0dc78e85ba7f7219db14e8152b8ea45476c5b8
hszalwinski <hszalwinski@pgs-soft.com>
2019-09-10 23:42:21 +0200

    Workshop WIP

--- tools/charts/comparison.py
+++ tools/charts/comparison.py
@@ -9,12 +9,12 @@ from tools.file_operations import load_csv_file
 
 class ComparisonChart(BaseChart):
     def build(self, drawable_stats: List[DrawableStats], filename: str, aggregation_type: str):
-        aggregation_type = AggregatorType(aggregation_type)
+        aggregator_type = AggregatorType(aggregation_type)
         figure_data = []
         for ds in drawable_stats:
             _, results = load_csv_file(ds.stats_path)
             chart_data = self._get_chart_data_from_csv_results(results)
-            x, y = self._get_data_to_plot(chart_data, aggregation_type)
+            x, y = self._get_data_to_plot(chart_data, aggregator_type)
             figure_data.append(Scatter(x=x, y=y, mode='lines+markers', name=str(ds)))
 
         self._plot_chart(figure_data, filename)
---
+++ workshop/csv_to_json.py
@@ -0,0 +1,20 @@
+import json
+from pathlib import Path
+
+from tools.file_operations import load_csv_file
+
+csv_path = Path('workshop/locations.csv')
+json_path = Path('workshop/locations.json')
+
+header, rows = load_csv_file(csv_path, delimiter=',')
+i = 1
+json_content = []
+for row in rows:
+    json_content.append({
+        'locationId': i,
+        'latitude': float(row['latitude']),
+        'longitude': float(row['longitude'])
+    })
+    i += 1
+
+json_path.write_text(json.dumps(json_content))
